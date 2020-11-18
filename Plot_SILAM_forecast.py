39da64f6c3e74e58ac71cb778b1c2bc394a74bb7
Rostislav Kouznetsov <rostoslav.kouznetsov@fmi.fi>
2020-11-18 10:06:24 +0200

    Populate with scripts

---
+++ Plot_SILAM_forecast.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python3
+
+import datetime as dt
+import subprocess as subp
+import atexit
+import urllib.request, urllib.error, urllib.parse
+import signal
+import os
+import sys
+
+"""
+# Pretend that the forecast starts at the fcdate
+# Actual date of global forecast is globfcdate
+
+"""
+
+# Get environment
+OUTPUT_DIR=os.getenv("OUTPUT_DIR",None)
+fcdate=os.getenv("fcdate",None)
+globfcdate=os.getenv("globfcdate",None)
+maxhours=int(os.getenv("maxhours",None))
+email=os.getenv("email",None)
+lonrange=os.getenv("lonrange",None)
+latrange=os.getenv("latrange",None)
+domain=os.getenv("suitename",None)
+#=os.getenv("",None)
+#=os.getenv("",None)
+
+
+
+#urlbase="https://silam.fmi.fi/thredds/ncss/grid/silam_glob_v5_7_1/runs/silam_glob_v5_7_1_RUN_"
+urlbase=os.getenv("urlpref",None)
+lons=lonrange.split(',')
+lats=latrange.split(',')
+bbox="spatial=bb&north=%s&west=%s&east=%s&south=%s"%(lats[1],lons[0],lons[1],lats[0]);
+# list of variables and levels according to MKD AQ index
+varlist="cnc_PM2_5 cnc_PM10 cnc_O3_gas cnc_NO2_gas cnc_SO2_gas cnc_CO_gas".split() 
+#varlist="cnc_PM2_5".split() 
+levs=dict(cnc_PM2_5="3 5 7.5 15 30 55 110", 
+          cnc_PM10="3 6 12 25 50 90 180", 
+          cnc_O3_gas="10 20 40 60 120 180 240", 
+          cnc_NO2_gas="10 20 40 60 120 180 240", 
+          cnc_SO2_gas="7 15 30 50 100 350 500",
+          cnc_CO_gas="750 1500 3000 5000 7500 10000 20000",
+          )
+		  
+#Names for output
+shortvar=dict(cnc_PM2_5="PM2_5", 
+          cnc_PM10="PM10", 
+          cnc_O3_gas="O3", 
+          cnc_NO2_gas="NO2", 
+          cnc_SO2_gas="SO2",
+          cnc_CO_gas="CO",
+          )
+# area to be plotted: Macedonian domain
+#bbox="spatial=bb&north=43.5&west=19&east=24.5&south=40"
+# descriptor of the user requesting the data, mail of the contact person in case someone changes on FMI side
+requestmark="email="+email
+# dates for the request 
+one_hour=dt.timedelta(hours=1)
+basedate=dt.datetime.strptime(globfcdate,"%Y%m%d")
+tstart=dt.datetime.strptime(fcdate,"%Y%m%d")
+tend=tstart + dt.timedelta(hours=maxhours)
+runTstr =basedate.strftime("%FT00:00:00Z")
+startTstr=tstart.strftime("%FT00:00:00Z")
+endTstr=tend.strftime("%FT00:00:00Z")
+# request the data
+URL="%s%s?var=%s&temporal=range&time_start=%s&time_end=%s&%s&vertCoord=12&accept=netcdf&%s"%(urlbase,runTstr,",".join(varlist),startTstr,endTstr,bbox,requestmark)
+
+#print(URL)
+#
+# Create directory to where the pictures should be stored for publishing
+#
+tstr=tstart.strftime("%Y%m%d")
+picdir=OUTPUT_DIR+"/webloads/%s/"%(fcdate)
+ncdir=OUTPUT_DIR+"/%s/"%(fcdate)
+try:
+    os.makedirs(picdir)
+    print("Creating directory for today's pictures "+picdir)
+except OSError:
+    if not os.path.isdir(picdir):
+        raise
+try:
+    os.makedirs(ncdir)
+    print("Creating directory for today's netcdf files "+ncdir)
+except OSError:
+    if not os.path.isdir(ncdir):
+        raise
+
+def killchild(child):
+    try:
+        child.kill()
+    except:
+        pass
+
+#
+# Check if the data is available at the THREAD server at FMI
+#
+print ("Checking if NetCDF files are available at FMI")
+
+ncfile=ncdir+"/SILAM4%s-%s.nc"%(domain,tstart.strftime("%Y%m%d"))
+
+if os.path.exists(ncfile):
+    print("Already exists: "+ ncfile)
+else:
+    print("Getting "+ ncfile)
+
+    for  attempts in range(3):
+
+        try:
+            response = urllib.request.urlopen(URL) #, timeout = 600)
+            with open( ncfile+'tmp', 'wb' ) as f:
+                f.write( response.read() )
+            os.rename(ncfile+'tmp',ncfile)
+            print(ncfile + " created!")
+            
+            break
+        except urllib.error.URLError as e:
+            attempts += 1
+            print("Download attampt %d failes"%(attempts))
+            print(type(e))
+
+    if attempts > 2:
+        print ("Failed to get the data")
+        sys.exit(255)
+
+#
+# Plot the results with GrADs
+# 
+print('Creating pictures at '+picdir)
+
+#Taken from the v5_4 script (It Works!)
+gradsbin=os.getenv("gradsnc", "/usr/bin/grads")
+gradsp=subp.Popen((gradsbin+" -b  -l").split(), shell=False, bufsize=100000, stdin=subp.PIPE, stdout=subp.PIPE)
+
+grads_scr="""
+
+set gxout grfill
+set mpdset hires
+## draw shoreline
+*set mpt 1 1 1 1
+* grid off
+* set mpt 2 off
+** Inland borders thick dark green
+*   'set rgb  250  0  50  0'
+**   'set mpt 3 15 1 2'
+*   'set mpt 3 250 1 2'
+* Thin short dash
+* set mpt 3 1 1 6
+* rivers -- blue 
+* set rgb  251  200  200  255
+* set mpt 4 4 1 0.1
+
+set mproj scaled
+*set xlab off
+*set ylab off
+*set frame off
+set rbcols 11 5 13 10 3 7 8 9
+#set parea 0 11 0 8.5
+sdfopen %(ncfile)s
+"""%dict(ncfile=ncfile)
+for it in range((tend-tstart).days*24 + 1):
+   #print(t)
+   plottime = tstart + dt.timedelta(hours=it)
+   gradstime=plottime.strftime("%H:%MZ%d%b%Y")
+   date=plottime.strftime("%d%b%Y")
+   hour=plottime.strftime("%H")
+   for v in varlist:
+     outname=picdir+shortvar[v]+"_surf_%(t)03d.png"%dict(t=it)
+     #print(outname)
+     grads_scr += """
+                set t %(t)d 
+                set clevs %(levs)s
+                set grads off
+                d  %(v)s
+                labels
+                cbar
+                draw title %(vS)s concentration (ug/m3), %(date)s hour: %(hour)s 
+                printim %(outname)s x800 y600 white
+                clear
+              """%dict(t=it+1, v=v, vS=shortvar[v], date=date , hour=hour, outname=outname, levs=levs[v])
+grads_scr += 'quit\r\n'
+
+
+#with open("tmp.grads_in","w") as f:
+#    f.write(grads_scr)
+
+#os.unlink(ncfile)
+
+
+gradsout,gradserr = gradsp.communicate(grads_scr.encode('utf-8'))
+print(gradserr)
+#os.unlink(ncfile)
+
+if  os.path.isfile(outname):
+#if  os.path.exists(outname):
+    print ("pictures created in %s"%(picdir))
+else:
+    print ("grads failed!")
+    os.system('rm -r '+picdir+' '+ncdir)
+    raise IOError("grads failed!")  
+
---
+++ make_pictures.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -u -e
+
+# put logo if corresponding command is provided
+#cd $scriptdir/delme || exit 234
+if [  -z ${PUTLOGOCMD+x}  ]; then
+   echo PUTLOGOCMD is not set
+else
+   echo Putting logos
+   ls ${picture_dir}/*.png |grep -v AQI_ |grep -v POLI_| xargs  -I XXX -P 10 ${PUTLOGOCMD} XXX XXX  
+   if [  -n ${PUTLOGOCMDINDEX+x}  ]; then
+      #separate logo for AQI
+      ls ${picture_dir}/*[AO][QL]I_???.png | xargs  -I XXX -P 10 ${PUTLOGOCMDINDEX} XXX XXX  
+   fi
+   echo compressing pics
+   ls ${picture_dir}/*.png  | xargs  -I XXX -P $nproc convert XXX PNG8:XXX
+fi
+#echo waiting..
+#wait
+
+
+
+echo Done with logos!
+[ -n "$outsuff" ] && exit 0 #No publish for modified runs
+
+
+if $publish; then
+    keepdays=7
+    pushd $picture_dir/..
+    for d in `find . -type d -name '20??????'|sort|head -n -$keepdays`; do
+       echo removing $d
+       rm -rf $d
+    done
+    ii=0
+    for d in `find . -type d -name '20??????'|sort -r`; do
+       linkname=`printf  %03d $ii`
+       rm -f $linkname
+       echo ln -s  $d $linkname
+       ln -sf  $d $linkname
+       ii=`expr $ii + 1`  
+    done
+
+    #deploy animation if not yet...
+    rsync -av $scriptdir/www/*.html .
+    if [ !  -d Napit ]; then
+     tar -xvf  $scriptdir/www/Napit.tar
+    fi
+
+    popd
+    fmi_data_path=eslogin:/fmi/data/silam.fmi.fi/partners/KAZ
+    echo Syncing $outputdir/webloads to $fmi_data_path
+#    mkdir -p $fmi_data_path
+    $rsync -a --delete  $outputdir/webloads/* $fmi_data_path/
+fi
+exit 0
+
