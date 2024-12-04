de91c0f387d47758ef2e6ec6d2ab483330e98b60
Rostislav Kouznetsov <rostislav.kouznetsov@fmi.fi>
2024-12-04 08:47:35 +0200

    KGZ local setup

--- make_pictures.sh
+++ make_pictures.sh
@@ -54,7 +54,7 @@ if $publish; then
     fi
 
     popd
-    fmi_data_path=eslogin:/fmi/data/silam.fmi.fi/partners/$suitename
+    fmi_data_path=/var/www/html
     echo Syncing $OUTPUT_DIR/webloads to $fmi_data_path
 #    mkdir -p $fmi_data_path
     rsync -a --delete  $OUTPUT_DIR/webloads/* $fmi_data_path/
